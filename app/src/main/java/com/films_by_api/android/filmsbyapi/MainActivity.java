3c19c129bc6894c02d990c13651889411e2deaf9
erdemcil <erdemchill@gmail.com>
2016-08-03 17:43:46 +0300

    First Commit

---
+++ app/src/main/java/com/films_by_api/android/filmsbyapi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.films_by_api.android.filmsbyapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
